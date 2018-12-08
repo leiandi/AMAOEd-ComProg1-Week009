edfe7ee239e3fc847ef7db5d99be2327ee216f7e
leiandi <43455219+leiandi@users.noreply.github.com>
2018-12-08 17:00:07 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//Name: Jameson Villaluna
+//Date: October 7, 2018
+#include <iostream>
+#include <string>
+using std::cout;
+using std::cin;
+using std::string;
+
+int main() {
+    string section;
+    cout << "Please enter your section: ";
+    cin >> section;
+    cout << "***Section: " << section << "'s section***";
+  system("pause");
+}
